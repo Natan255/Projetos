fa6d9599391608abac8f9c062d9858807627cd18
Natan <131617471+Natan255@users.noreply.github.com>
2023-12-22 17:45:26 -0300

    progresso
    fiz uma tela de finalização da partida e inverte o timer, ta quase pronto essa parte já

--- pontaria/pontaria_jogar.js
+++ pontaria/pontaria_jogar.js
@@ -1,32 +1,62 @@
-
 let contador = 1
-let min = 0
+let min = 1
 let sec_unidade = 0
 let sec_dezena = 0
 let score = -1
 
+function fim(){
+    let alvos = document.getElementById('alvos')
+    let pontuacao = document.getElementById('pontuacao')
+    let timer =  document.getElementById('timer')
+    let tempo = document.getElementById('tempo')
+ 
+    
+    tempo.style.display = 'none'
+    pontuacao.style.display = 'none'
+    alvos.style.display = "none"
+    timer.style.display = ""
+    timer.innerHTML = `Fim! <br> Pontuação: ${score} <br> <button id='menu'>Menu</button> `
+
+    
+
+}
 
 
 function cronometro(){
-    
+    let alvos = document.getElementById('alvos')
     let tempo = document.getElementById('tempo')
-
-    if (sec_dezena == 6){
-        min += 1
+    let timer =  document.getElementById('timer')
+    if (sec_dezena == 0 && sec_unidade == -1 && min == 0 ){
         sec_dezena = 0
+        sec_unidade = 0
+        min = 0
+        alvos.disabled = true
+        fim()
+        cronometro= saf
+        
+        
     }
     
-    if (sec_unidade == 10){
-        sec_unidade = 0
-        sec_dezena += 1
+    if (sec_dezena == 0 && sec_unidade == -1){
+        min -= 1
+        sec_dezena = 5
+        sec_unidade = 9
+        if (min < 0){
+            min += 1
+        }
+    }
+    
+    if (sec_unidade == -1){
+        sec_unidade = 9
+        sec_dezena -= 1
         cronometro()
     }else{
         tempo.innerHTML ='Tempo:    :'
         tempo.innerHTML += ` 0${min}:${sec_dezena}${sec_unidade}`
-        sec_unidade += 1
+        sec_unidade -= 1
         setTimeout(cronometro, 1000)
-
     }
+    
 }
 function ativar(){
     let alvos = document.getElementById('alvos')
@@ -37,25 +67,21 @@ function ativar(){
     let sort_left = Math.floor(Math.random() * 95)
     alvos.style.top = `${sort_top}vh`
     alvos.style.left = `${sort_left}vw`
+    score += 1
+    pontuacao.innerHTML = `Pontuação: ${score}`
 }
 
 function alvo(){
-    let pontuacao = document.getElementById('pontuacao')
-    
-    score += 1
-    let div = "<button id='alvos' onclick='alvo()'><img src='imagens/logo_mira.png' width='0' height='0' id='imagem_mira'></button>"
+    let div = "<button id='alvos' onclick='ativar()'><img src='imagens/logo_mira.png' width='0' height='0' id='imagem_mira'></button>"
     document.querySelector("body").insertAdjacentHTML("beforeend", div)
     console.log('foi')
-    pontuacao.innerHTML = `Pontuação: ${score}`
     ativar()
-    
     
 }
 
 function tempo(){
     let timer =  document.getElementById('timer')
     let conteiner = document.getElementById('conteiner')
-
     timer.innerHTML = `${contador}`
     contador += 1
     if (contador <= 4){
@@ -66,12 +92,9 @@ function tempo(){
         cronometro()
         alvo()
         
-        
     }
        
 }   
-
-
 
 tempo()
 
