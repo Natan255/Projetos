14584184567e69b1dd6dbc2bbeeb8c2eebfec105
Natan <131617471+Natan255@users.noreply.github.com>
2023-12-22 00:16:25 -0300

    avanço
    adicionei o alvo e o sistema de pontuação.

--- pontaria/pontaria_jogar.js
+++ pontaria/pontaria_jogar.js
@@ -1,7 +1,11 @@
+
 let contador = 1
 let min = 0
 let sec_unidade = 0
 let sec_dezena = 0
+let score = -1
+
+
 
 function cronometro(){
     
@@ -24,18 +28,45 @@ function cronometro(){
 
     }
 }
+function ativar(){
+    let alvos = document.getElementById('alvos')
+    let imagem_mira = document.getElementById('imagem_mira')
+    imagem_mira.style.width ="120%"
+    imagem_mira.style.height ="110%"
+    let sort_top = Math.floor(Math.random() * 85)
+    let sort_left = Math.floor(Math.random() * 95)
+    alvos.style.top = `${sort_top}vh`
+    alvos.style.left = `${sort_left}vw`
+}
+
+function alvo(){
+    let pontuacao = document.getElementById('pontuacao')
+    
+    score += 1
+    let div = "<button id='alvos' onclick='alvo()'><img src='imagens/logo_mira.png' width='0' height='0' id='imagem_mira'></button>"
+    document.querySelector("body").insertAdjacentHTML("beforeend", div)
+    console.log('foi')
+    pontuacao.innerHTML = `Pontuação: ${score}`
+    ativar()
+    
+    
+}
 
 function tempo(){
     let timer =  document.getElementById('timer')
     let conteiner = document.getElementById('conteiner')
+
     timer.innerHTML = `${contador}`
-    contador +=1
+    contador += 1
     if (contador <= 4){
         setTimeout(tempo, 1000) 
     }else{
         timer.style.display = 'none'
         conteiner.style.color = 'white'
         cronometro()
+        alvo()
+        
+        
     }
        
 }   
