2a340f121b3f7e68d19494b18b24e93eede65cfd
Natan <natanrnascimento@gmail.com>
2025-09-06 11:21:14 -0300

    Ajuste do deslocamento da moeda, aplicando equação de função linear para isso

--- peteléco/Campo.java
+++ peteléco/Campo.java
@@ -1,7 +1,5 @@
 import javax.swing.JPanel;
 
-import org.w3c.dom.events.MouseEvent;
-
 import java.awt.*;
 
 //criar campo separada da criação da janela melhorou a velocidade do redimensionamento do desenho
@@ -9,9 +7,13 @@ import java.awt.*;
 
 public class Campo extends JPanel {
 
-    int moeda_X;
-    int moeda_Y;
-    double velocidade = 0.01;
+    double moeda_X;
+    double moeda_Y;
+    double b;
+    double velocidade = 0.1;
+    double coeficienteAngular;
+    double deltaX;
+    double deltaY;  
     int mlinha_X;
     int mlinha_Y;
     private boolean moedaClicada = false;
@@ -54,6 +56,8 @@ public class Campo extends JPanel {
             public void mouseReleased(java.awt.event.MouseEvent e) {
                 if (moedaClicada == true) {
                     moedaLancada = true;
+                    coeficienteAngular = (double) deltaY / deltaX;
+                    b = moeda_Y - (coeficienteAngular * moeda_X);
                     moedaClicada = false;
                     System.out.println("Moeda lançada");
                 }
@@ -135,34 +139,46 @@ public class Campo extends JPanel {
         // Moeda ---------------------------- 
 
         g.setColor(Color.BLACK);
-        g.fillOval(moeda_X, moeda_Y, raio * 4, raio * 4);
+        g.fillOval((int)moeda_X, (int)moeda_Y, raio * 4, raio * 4);
+        deltaX = mlinha_X - (moeda_X + raio*2);
+        deltaY = mlinha_Y - (moeda_Y + raio*2);
+        
+        
 
         if (moedaClicada == true) {
-            g.drawLine(moeda_X + raio * 2, moeda_Y + raio * 2, mlinha_X, mlinha_Y);
+            g.drawLine((int)moeda_X + raio * 2, (int)moeda_Y + raio * 2, mlinha_X, mlinha_Y);
             repaint();
             
         }
         if (moedaLancada == true) {
-            if (moeda_X + raio * 2 <= mlinha_X) {
-                moeda_X -= 1 * velocidade;
-            }else{
-                moeda_X += 1 * velocidade;
+            if (deltaY > 1) {
+
+                moeda_Y = coeficienteAngular * moeda_X + b;
+                
+            } else if (deltaY < -1) {
+
+                moeda_Y = coeficienteAngular * moeda_X + b;       
             }
-            if (moeda_Y + raio * 2 <= mlinha_Y) {
-                moeda_Y -= 1 * velocidade; 
+
+            if (moeda_X  < mlinha_X) {
+                moeda_X -= velocidade;  
+                
             }else{
-                moeda_Y += 1 * velocidade;
+                moeda_X += velocidade;  
+                
             }
+
+
             
-            
-            if ((moeda_Y - raio <= altura / 7) || (moeda_Y + raio >= altura - altura / 8)) {
+            if ((moeda_Y - raio <= altura / 7) || (moeda_Y + raio >= altura - altura / 8) || (moeda_X - raio <= 0)
+                    || (moeda_X + raio >= largura)) {
                 System.out.println("Moeda parou");
                 moedaClicada = false;
                 moedaLancada = false;
                 
                 
             }
-            System.out.printf("Moeda em: (%d, %d)%n", moeda_X, moeda_Y);
+            System.out.printf("Moeda em: (%.2f, %.2f)%n", moeda_X, moeda_Y);
             repaint();
         }
         
