567f3d9e7e1032052225ff2d9985a168c4fd2697
Natan <natanrnascimento@gmail.com>
2025-09-09 21:52:14 -0300

    Implementação dos pregos e suas colisoes

--- peteléco/Campo.java
+++ peteléco/Campo.java
@@ -61,14 +61,12 @@ public class Campo extends JPanel {
                 if (moedaClicada == true) {
                     moedaLancada = true;
                     moedaClicada = false;
-                    if(largura >= 1300){
-                        velocidade = modulo * 0.0107;
-                    }else{
+                    if (largura >= 1300) {
+                        velocidade = modulo * 0.007;
+                    } else {
                         velocidade = modulo * 0.001; // use um valor maior para testar
                     }
-                    
                 }
-
                 repaint();
             }
 
@@ -79,6 +77,8 @@ public class Campo extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(5));
 
         largura = getWidth();
         altura = getHeight();
@@ -92,7 +92,6 @@ public class Campo extends JPanel {
 
         }
 
-        // Cor de fundo do campo
         g.setColor(new Color(0, 128, 0));
         g.fillRect(0, 0, largura, altura);
         g.setColor(Color.WHITE);
@@ -140,13 +139,77 @@ public class Campo extends JPanel {
         // Meio de campo ----------------------
 
         g.fillOval(largura / 2 - raio, altura / 2 - raio, raio * 2, raio * 2);
-        g.drawOval(largura / 2 - raio * 11, altura / 2 - raio * 11, raio * 22, raio * 22);
+        g.drawOval(largura / 2 - 55, altura / 2 - 55, 110, 110);
         g.drawLine(largura / 2, altura / 7, largura / 2, altura - altura / 8);
 
         // Moeda ----------------------------
 
+        g.setColor(Color.RED);
+        g.fillOval((int) moeda_X, (int) moeda_Y, raio * 4, raio * 4); // problema de posicionamento da moeda quando a
+                                                                      // tela é redimensionada
+
+        // ---------------------------------------------------------------------------------------
+        // Pregos (posições dos jogadores)
         g.setColor(Color.BLACK);
-        g.fillOval((int) moeda_X, (int) moeda_Y, raio * 4, raio * 4); //problema de posicionamento da moeda quando a tela é redimensionada
+        int raioPrego = Math.min(largura, altura) / 35;
+
+        // ---------------- TIME DA ESQUERDA ----------------
+
+        // Goleiro colado no gol
+        int xGoleiroEsq = largura / 25;
+        int yGoleiroEsq = altura / 2;
+        g.fillOval(xGoleiroEsq - raioPrego, yGoleiroEsq - raioPrego, raioPrego * 2, raioPrego * 2);
+
+        // Linha de 2 zagueiros praticamente na frente do goleiro
+        int xZagueirosEsq = largura / 12;
+        g.fillOval(xZagueirosEsq - raioPrego, altura / 3 - raioPrego, raioPrego * 2, raioPrego * 2);
+        g.fillOval(xZagueirosEsq - raioPrego, 2 * altura / 3 - raioPrego, raioPrego * 2, raioPrego * 2);
+
+        // Linha de 4 defensores (fechando quase toda a largura do campo)
+        int xDefensoresEsq = largura / 6;
+        g.fillOval(xDefensoresEsq - raioPrego, altura / 5 - raioPrego, raioPrego * 2, raioPrego * 2);
+        g.fillOval(xDefensoresEsq - raioPrego, 2 * altura / 5 - raioPrego, raioPrego * 2, raioPrego * 2);
+        g.fillOval(xDefensoresEsq - raioPrego, 3 * altura / 5 - raioPrego, raioPrego * 2, raioPrego * 2);
+        g.fillOval(xDefensoresEsq - raioPrego, 4 * altura / 5 - raioPrego, raioPrego * 2, raioPrego * 2);
+
+        // Meio-campo mais recuado
+        int xMeiasEsq = largura / 3;
+        g.fillOval(xMeiasEsq - raioPrego, altura / 3 - raioPrego, raioPrego * 2, raioPrego * 2);
+        g.fillOval(xMeiasEsq - raioPrego, 2 * altura / 3 - raioPrego, raioPrego * 2, raioPrego * 2);
+
+        // Atacante solitário
+        int xAtacanteEsq = largura / 2 - largura / 8;
+        int yAtacanteEsq = altura / 2;
+        g.fillOval(xAtacanteEsq - raioPrego, yAtacanteEsq - raioPrego, raioPrego * 2, raioPrego * 2);
+
+        // ---------------- TIME DA DIREITA (espelhado) ----------------
+
+        int xGoleiroDir = largura - largura / 25;
+        int yGoleiroDir = altura / 2;
+        g.fillOval(xGoleiroDir - raioPrego, yGoleiroDir - raioPrego, raioPrego * 2, raioPrego * 2);
+
+        // Zagueiros perto do gol
+        int xZagueirosDir = largura - largura / 12;
+        g.fillOval(xZagueirosDir - raioPrego, altura / 3 - raioPrego, raioPrego * 2, raioPrego * 2);
+        g.fillOval(xZagueirosDir - raioPrego, 2 * altura / 3 - raioPrego, raioPrego * 2, raioPrego * 2);
+
+        // Linha de 4 defensores
+        int xDefensoresDir = largura - largura / 6;
+        g.fillOval(xDefensoresDir - raioPrego, altura / 5 - raioPrego, raioPrego * 2, raioPrego * 2);
+        g.fillOval(xDefensoresDir - raioPrego, 2 * altura / 5 - raioPrego, raioPrego * 2, raioPrego * 2);
+        g.fillOval(xDefensoresDir - raioPrego, 3 * altura / 5 - raioPrego, raioPrego * 2, raioPrego * 2);
+        g.fillOval(xDefensoresDir - raioPrego, 4 * altura / 5 - raioPrego, raioPrego * 2, raioPrego * 2);
+
+        // Meio-campo recuado
+        int xMeiasDir = largura - largura / 3;
+        g.fillOval(xMeiasDir - raioPrego, altura / 3 - raioPrego, raioPrego * 2, raioPrego * 2);
+        g.fillOval(xMeiasDir - raioPrego, 2 * altura / 3 - raioPrego, raioPrego * 2, raioPrego * 2);
+
+        // Atacante
+        int xAtacanteDir = largura / 2 + largura / 8;
+        int yAtacanteDir = altura / 2;
+        g.fillOval(xAtacanteDir - raioPrego, yAtacanteDir - raioPrego, raioPrego * 2, raioPrego * 2);
+        // ---------------------------------------------------------------------------------------
 
         if (moedaClicada == true) {
             g.drawLine((int) moeda_X + raio * 2, (int) moeda_Y + raio * 2, mlinha_X, mlinha_Y);
@@ -155,39 +218,33 @@ public class Campo extends JPanel {
         }
         if (moedaLancada) {
             // Atualiza posição
-        
-            
             moeda_X += -(deltaX / modulo) * velocidade; // normaliza e multiplica pela velocidade
             moeda_Y += -(deltaY / modulo) * velocidade;
-            if (largura < 1000){
-                velocidade -=  0.00001; // diminui a velocidade a cada atualização
-            } else if (largura >= 1000 && largura <= 1300){
-                velocidade -=  0.0001; // diminui a velocidade a cada atualização
-            } else if (largura > 1300){
-                velocidade -=  0.003; // diminui a velocidade a cada atualização
+
+            // Redução da velocidade
+            if (largura < 1000) {
+                velocidade -= 0.00001;
+            } else if (largura >= 1000 && largura <= 1300) {
+                velocidade -= 0.0001;
+            } else if (largura > 1300) {
+                velocidade -= 0.0012;
             }
-            
 
-
-            // Colisão com a parede esquerda (exceto se estiver no gol)
+            // ------------------ Colisão com paredes ------------------
             if ((moeda_X < 0) && !colidiu) {
-                // Só colide se NÃO estiver na área do gol esquerdo
                 if (!(moeda_Y + raio * 4 > golY_esq && moeda_Y < golY_esq + golAltura)) {
                     deltaX = -deltaX;
                     colidiu = true;
                 }
             }
 
-            // Colisão com a parede direita
             if ((moeda_X + raio * 4 >= largura) && !colidiu) {
-
                 if (!(moeda_Y + raio * 4 > golY_esq && moeda_Y < golY_esq + golAltura)) {
                     deltaX = -deltaX;
                     colidiu = true;
                 }
             }
 
-            // Colisão com as paredes de cima e baixo
             if ((moeda_Y - raio * 2 <= altura / 7) && !colidiu) {
                 deltaY = -deltaY;
                 colidiu = true;
@@ -197,30 +254,67 @@ public class Campo extends JPanel {
                 colidiu = true;
             }
 
-            // Gol para o time da direita
+            // ------------------ Colisão com jogadores (pregos) ------------------
+            int[][] jogadores = {
+                    // Time Esquerda
+                    { xGoleiroEsq, yGoleiroEsq },
+                    { xZagueirosEsq, altura / 3 }, { xZagueirosEsq, 2 * altura / 3 },
+                    { xDefensoresEsq, altura / 5 }, { xDefensoresEsq, 2 * altura / 5 },
+                    { xDefensoresEsq, 3 * altura / 5 }, { xDefensoresEsq, 4 * altura / 5 },
+                    { xMeiasEsq, altura / 3 }, { xMeiasEsq, 2 * altura / 3 },
+                    { xAtacanteEsq, altura / 2 },
+                    // Time Direita
+                    { xGoleiroDir, yGoleiroDir },
+                    { xZagueirosDir, altura / 3 }, { xZagueirosDir, 2 * altura / 3 },
+                    { xDefensoresDir, altura / 5 }, { xDefensoresDir, 2 * altura / 5 },
+                    { xDefensoresDir, 3 * altura / 5 }, { xDefensoresDir, 4 * altura / 5 },
+                    { xMeiasDir, altura / 3 }, { xMeiasDir, 2 * altura / 3 },
+                    { xAtacanteDir, altura / 2 }
+            };
+
+            for (int[] jogador : jogadores) {
+                double dx = moeda_X + raio * 2 - jogador[0];
+                double dy = moeda_Y + raio * 2 - jogador[1];
+                double dist = Math.sqrt(dx * dx + dy * dy);
+                if (dist < raio + raioPrego) {
+                    // Normal da colisão (vetor unitário do jogador para a moeda)
+                    double nx = dx / dist;
+                    double ny = dy / dist;
+
+                    // Componente da velocidade ao longo da normal
+                    double velNormal = (deltaX * nx + deltaY * ny);
+
+                    // Reflete a velocidade em relação à normal
+                    deltaX = deltaX - 2 * velNormal * nx;
+                    deltaY = deltaY - 2 * velNormal * ny;
+
+                    colidiu = true;
+                    break;
+                }
+            }
+
+            // ------------------ Gol ------------------
             if (moeda_Y > golY_esq && moeda_Y < golY_esq + golAltura && moeda_X <= -raio * 4) {
                 timeDireita++;
                 colidiu = false;
                 moedaLancada = false;
-                moedaInicializada = false; // pra resetar a posição da moeda
+                moedaInicializada = false;
                 System.out.println("GOL PARA O TIME DA DIREITA!");
                 System.out.println("Placar: " + timeEsquerda + " x " + timeDireita);
-
             }
+
             if (moeda_Y > golY_esq && moeda_Y < golY_esq + golAltura && moeda_X >= largura) {
+                timeEsquerda++;
                 colidiu = false;
                 moedaLancada = false;
                 moedaInicializada = false;
-                timeEsquerda++;
                 System.out.println("GOL PARA O TIME DA ESQUERDA!");
                 System.out.println("Placar: " + timeEsquerda + " x " + timeDireita);
-
             }
-            
+
             if (velocidade <= 0) {
                 velocidade = 0;
                 moedaLancada = false;
-
             }
             if (colidiu) {
                 colidiu = false;
@@ -228,6 +322,7 @@ public class Campo extends JPanel {
 
             repaint();
         }
+
         // Placar ----------------------------
         g.setColor(Color.BLACK);
         g.setFont(new Font("Arial", Font.BOLD, 55));
